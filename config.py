a1303bac72585b5dbf3dd0de651f2df489750131
henglay <henglay@example.com>
2026-02-24 00:37:36 +0700

    new sqlite and migration file

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL")
-        or "mysql+pymysql://root:Henglay699$@localhost:3306/user_db"
+        or "sqlite:///" + os.path.join(BASE_DIR,"instance","users.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
